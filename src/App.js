f7a8546258a57b80be2581dceb79e7cfbd85c2f7
Elio Lu <1520622848@qq.com>
2021-10-01 13:57:49 +0800

    feat: :octocat: useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function SecretComponent() {
-  return <h1>Secret information for authorized userd only</h1>
-}
+function App() {
+  const [emotion, setEmotion] = useState("happy")
+  const [secondary, setSecondary] = useState("tired")
 
-function RegularComponent() {
-  return <h1>Everyone can see this components</h1>
-}
+  useEffect(() => {
+    console.log(`It's ${secondary} around here`)
+  }, [secondary])
 
-// Object destructuring
-function App({authorized}) {
+  // second parameter dependency array will watch state values whatever in that array
+  // Only run when watching variables have changed
+  useEffect(() => {
+    console.log(`It's a ${emotion} around here`)
+  }, [emotion])
   return (
     <>
-      {authorized ? <SecretComponent /> : <RegularComponent />}
+      <h1>Hey, current emotion is {emotion} and {secondary}</h1>
+      <button onClick={() => setEmotion("happy")}>Happy</button>
+      <button onClick={() => setEmotion("enthusiastic")}>Enthusiastic</button>
+      <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
+      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
     </>
   )
 }
